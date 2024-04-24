184a558d77edaa22925e11ed9764c0579b942fa1
Lucas-SFernandez <127568078+Lucas-SFernandez@users.noreply.github.com>
2024-04-24 18:52:44 -0300

    finalização do projeto Alura mid

--- index.js
+++ index.js
@@ -1,9 +1,74 @@
-function tocarSom () {
+function somPom() {
 
     document.querySelector('#som_tecla_pom').play();
+
 }
 
+function somClep() {
 
+    document.querySelector('#som_tecla_clap').play();
 
-document.querySelector('.tecla_pom');
+}
 
+function somTim() {
+
+    document.querySelector('#som_tecla_tim').play();
+
+}
+
+function somPuff() {
+
+    document.querySelector('#som_tecla_puff').play();
+
+}
+
+function somSplash() {
+
+    document.querySelector('#som_tecla_splash').play();
+
+}
+
+function somToim() {
+
+    document.querySelector('#som_tecla_toim').play();
+
+}
+
+function somPsh() {
+
+    document.querySelector('#som_tecla_psh').play();
+
+}
+
+function somTic() {
+
+    document.querySelector('#som_tecla_tic').play();
+
+}
+
+function somTom() {
+
+    document.querySelector('#som_tecla_tom').play();
+
+}
+
+const listaDeTeclas = 
+document.querySelectorAll('.tecla');
+
+listaDeTeclas [0].onclick = somPom;
+
+listaDeTeclas [1].onclick = somClep;
+
+listaDeTeclas [2].onclick = somTim;
+
+listaDeTeclas [3].onclick = somPuff;
+
+listaDeTeclas [4].onclick = somSplash;
+
+listaDeTeclas [5].onclick = somToim;
+
+listaDeTeclas [6].onclick = somPsh;
+
+listaDeTeclas [7].onclick = somTic;
+
+listaDeTeclas [8].onclick = somTom;
